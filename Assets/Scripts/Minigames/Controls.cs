13e85449b19f9b3b407d642c372c0f617cf0ddc8
Samburgerman <104079001+Samburgerman@users.noreply.github.com>
2022-09-30 21:54:10 -0500

    Working on the 1st minigame
    Minor bug fixes but still inputs do nothing in the scene.

--- Assets/Scripts/Minigames/Controls.cs
+++ Assets/Scripts/Minigames/Controls.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Controls : MonoBehaviour
 {
     public int playerNum = 0;//the player numbers are 0 through 3.
-    public static string[] axisHoriz;//input axis in the build setting of the project
-    public static KeyCode[] jump;//input jump buttons in the build setting of the project
-    public static KeyCode[] atk;//input attack or interact buttons in the build setting of the project
+    public static string[] axisHoriz= {"Horizontal0", "Horizontal1", "Horizontal2", "Horizontal3" };//input axis in the build setting of the project
+    public static string[] jump= { "Jump0", "Jump1", "Jump2", "Jump3" };//input jump buttons in the build setting of the project
+    public static string[] atk = { "Atk0", "Atk1", "Atk2", "Atk3" };//input attack or interact buttons in the build setting of the project
     public float speed = 10.0f;//the speed of horizontal movement
     public float maxSpeed= 100.0f;//the max speed x or y the player can experience
     public float jumpSize = 10.0f;
@@ -22,14 +22,15 @@ public class Controls : MonoBehaviour
     {
         float xForce = Input.GetAxis(axisHoriz[playerNum]) * speed;
         float yForce = 0;
-        if (Input.GetKey(jump[playerNum]))
+        if (Input.GetAxis(jump[playerNum])!=0)
         {
+            print("jump it: "+Input.GetAxis(jump[playerNum]));
             yForce += jumpSize;
         }
         Vector2 force = new Vector2(xForce, yForce);
         rb.AddForce(force);
         ClampVelocity(maxSpeed, maxSpeed);
-        if (Input.GetKey(atk[playerNum]))
+        if (Input.GetAxis(atk[playerNum]) != 0)
         {
             Attack();
         }
