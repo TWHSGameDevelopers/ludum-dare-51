a9495be8570f8fa7b9eeb6e3262c1a2b243b465b
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2022-10-01 10:31:07 -0500

    add ball.cs

--- Assets/Scripts/Minigames/Pong/Ball.cs
+++ Assets/Scripts/Minigames/Pong/Ball.cs
@@ -4,10 +4,51 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [Header("Ball")]
     public Rigidbody2D rb;
+    public int speed;
+
+    [Header("Camera")]
+    public Camera cam;
+
+    private void Launch()
+    {
+        int x = Random.Range(0, 2) == 0 ? speed : -speed;
+        int y = Random.Range(0, 2) == 0 ? speed : -speed;
+        rb.velocity = new Vector2(x, y);
+    }
+    private void Start()
+    {
+        Launch();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Paddle"))
-            rb.velocity = -rb.velocity;
+        Vector2 upRight = new Vector2(speed, speed);
+        Vector2 upLeft = new Vector2(-speed, speed);
+        Vector2 downRight = new Vector2(speed, -speed);
+        Vector2 downLeft = new Vector2(-speed, -speed);
+
+        if (collision.CompareTag("LateralPaddle"))
+        {
+            if (rb.velocity == upRight)
+                rb.velocity = downRight;
+            else if (rb.velocity == upLeft)
+                rb.velocity = downLeft;
+            else if (rb.velocity == downRight)
+                rb.velocity = upRight;
+            else
+                rb.velocity = upLeft;
+        }
+        else if (collision.CompareTag("VerticalPaddle"))
+        {
+            if (rb.velocity == upRight)
+                rb.velocity = upLeft;
+            else if (rb.velocity == upLeft)
+                rb.velocity = upRight;
+            else if (rb.velocity == downRight)
+                rb.velocity = downLeft;
+            else
+                rb.velocity = downRight;
+        }
     }
 }
