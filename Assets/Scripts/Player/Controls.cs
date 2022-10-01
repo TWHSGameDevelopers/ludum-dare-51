3c6d6569d0b26bee3505cdfea56dada9fbe681dc
Samburgerman <104079001+Samburgerman@users.noreply.github.com>
2022-10-01 12:25:38 -0500

    finished lava minigame + player script bug fixes
    bug fixes include better movement and resetting vertical velocty on jump

--- Assets/Scripts/Player/Controls.cs
+++ Assets/Scripts/Player/Controls.cs
@@ -9,13 +9,19 @@ public class Controls : MonoBehaviour
     static string[] jump= { "Jump0", "Jump1", "Jump2", "Jump3" };//input jump buttons in the build setting of the project
     static string[] atk = { "Atk0", "Atk1", "Atk2", "Atk3" };//input attack or interact buttons in the build setting of the project
 
+    public Vector2 startPos;
     public bool canInput = true;//can enable or disable player movement
     public float speed = 10.0f;//the speed of horizontal movement
     public float maxSpeed= 100.0f;//the max speed x or y the player can experience
+
+    public bool canJump = true;
     public float jumpSize = 50;
     public int maxJumps = 1;
     public int jumpsUsed = 0;
+    public float jumpLag = 0.3f;//coooldoown before one can jump again
     public float groundedDist = 0.5f;
+
+    public float friction = 0.1f; // 0.1 means lose 0.1 of momentum
 
     public Rigidbody2D rb;
     public GameObject floor;
@@ -23,25 +29,33 @@ public class Controls : MonoBehaviour
 
     private void Awake()
     {
+        startPos = transform.position;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if(canInput)
+        
+        if(canInput&&playerNum==0)//todo reenable all
         {
+            print(playerNum+" jump it: " + Input.GetAxis(jump[playerNum]));
             //print(Input.GetAxis(axisHoriz[playerNum]) + "||" + Input.GetAxis(jump[playerNum]) + "||"+Input.GetAxis(atk[playerNum]));
             float xForce = Input.GetAxis(axisHoriz[playerNum]) * speed;
             float yForce = 0;
-            if ((Input.GetAxis(jump[playerNum]) != 0)&&(jumpsUsed<maxJumps))
+            if ((Input.GetAxis(jump[playerNum]) > 0.1f) && (jumpsUsed < maxJumps) && canJump)
             {
+                //print("jump used");
+                StartCoroutine(JumpLag());
                 //print("jump it: " + Input.GetAxis(jump[playerNum]));
+                print("went past");
                 jumpsUsed++;
                 yForce += jumpSize;
+                rb.velocity = new Vector2(rb.velocity.x, 0);//resets vertical momentum
+                //print("yforce: " + yForce);
             }
             Vector2 force = new Vector2(xForce, yForce);
-            rb.AddForce(force);//this force is the sum of the jump and horizontal inputs
-            ClampVelocity(maxSpeed, maxSpeed);
+            rb.AddForce(force);//this force is the sum of the jump and horizontal input
+            //print(force + " added");
             if (Input.GetAxis(atk[playerNum]) != 0)
             {
                 Attack();
@@ -49,13 +63,26 @@ public class Controls : MonoBehaviour
         }
         if (Grounded())
         {
+            Friction();
             jumpsUsed = 0;
         }
+        ClampVelocity(maxSpeed, maxSpeed);
+    }
+
+    public void Friction()
+    {
+        rb.velocity *= (1.0f-friction);
     }
 
     public bool Grounded()
-    {
+    {/*
         if(transform.position.y-floor.transform.position.y<groundedDist)
+        {
+            return true;
+        }
+        return false;
+    */
+        if (Physics2D.Raycast(transform.position, Vector2.down, groundedDist, 64))//64 binary for 6 layermask
         {
             return true;
         }
@@ -64,14 +91,22 @@ public class Controls : MonoBehaviour
 
     public void Attack()
     {
-        Debug.Log("attack mwahaha");
+        //Debug.Log("attack mwahaha");
     }
     
     public Vector2 ClampVelocity(float x, float y)
     {
-    	Vector2 nu =  new Vector2(Mathf.Clamp(rb.velocity.x, -1*maxSpeed, maxSpeed),Mathf.Clamp(rb.velocity.y, -1*maxSpeed, maxSpeed));
+        //Vector2 nu = new Vector2(Mathf.Clamp(rb.velocity.x, -1 * maxSpeed, maxSpeed), Mathf.Clamp(rb.velocity.y, -1 * maxSpeed, maxSpeed));
+
+        Vector2 nu =  new Vector2(Mathf.Clamp(rb.velocity.x, -1*maxSpeed, maxSpeed),rb.velocity.y);
         rb.velocity = nu;
     	return rb.velocity;
     }
 
+    IEnumerator JumpLag()
+    {
+        canJump = false;
+        yield return new WaitForSeconds(jumpLag);
+        canJump = true;
+    }
 }
